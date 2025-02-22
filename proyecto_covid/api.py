6948c272670eb6ebd0d77eac1d4ca0e5b7699ca1
Pereaz <davperea486@gmail.com>
2025-02-21 21:08:54 -0500

    Delete proyecto_covid directory

--- proyecto_covid/api.py
+++
@@ -1,6 +0,0 @@
-from sodapy import Socrata
-
-def obtener_datos(departamento, limite):
-    cliente = Socrata("www.datos.gov.co", None)
-    resultados = cliente.get("gt2j-8ykr", departamento_nom=departamento, limit=limite)
-    return resultados
